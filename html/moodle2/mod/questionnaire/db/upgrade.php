86c7030ec127f17cd31a8a6191621db768617d89
Sara Arjona <sarjona@xtec.cat>
2012-12-27 12:17:43 +0100

    Added try/catch to avoid questionnaire_survey->name ddldependencyerror

--- html/moodle2/mod/questionnaire/db/upgrade.php
+++ html/moodle2/mod/questionnaire/db/upgrade.php
@@ -17,10 +17,14 @@ function xmldb_questionnaire_upgrade($oldversion) {
         // Changing precision of field name on table questionnaire_survey to (255).
 
         // First drop the index.
-        $table = new xmldb_table('questionnaire_survey');
-        $index = new xmldb_index('name');
-        $index->set_attributes(XMLDB_INDEX_NOTUNIQUE, array('name'));
-        $dbman->drop_index($table, $index);
+        //XTEC ************ AFEGIT - To avoid questionnaire_survey->name ddldependencyerror
+        //2012.12.27  @sarjona
+        try{
+        //************ FI
+            $table = new xmldb_table('questionnaire_survey');
+            $index = new xmldb_index('name');
+            $index->set_attributes(XMLDB_INDEX_NOTUNIQUE, array('name'));
+            $dbman->drop_index($table, $index);
 
         // Launch change of precision for field name.
         $field = new xmldb_field('name', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null, 'id');
@@ -31,6 +35,12 @@ function xmldb_questionnaire_upgrade($oldversion) {
         $index = new xmldb_index('name');
         $index->set_attributes(XMLDB_INDEX_NOTUNIQUE, array('name'));
         $dbman->add_index($table, $index);
+        //XTEC ************ AFEGIT - To avoid questionnaire_survey->name ddldependencyerror
+        //2012.12.27  @sarjona
+        } catch (Exception $e){
+            
+        }
+        //************ FI
 
         // questionnaire savepoint reached
         upgrade_mod_savepoint(true, 2012100800, 'questionnaire');
