3620ce4a79dd2ccc2cf1b27981a78a128b2c0263
Toni Ginard <aginard@xtec.cat>
2013-02-18 13:59:38 +0100

    Fixed potential XSS vulnerability

--- html/error.php
+++ html/error.php
@@ -11,6 +11,7 @@
 
 <?php
     include 'config/env-config.php';
+    include 'config/dblib-mysql.php';
     global $agora;
 ?>
 
@@ -26,7 +27,7 @@
                 <h1 class="titol imp">SERVEI &Agrave;GORA NO DISPONIBLE</h1>
                 <div class="content">
                     <div>
-                        <p>El servei &Agrave;gora no està disponible en aquests moments. Es treballa per solucionar els problemes t&egrave;cnics el més aviat possible.</p>
+                        <p>El servei &Agrave;gora no està disponible en aquests moments. Estem treballant per solucionar els problemes t&egrave;cnics el més aviat possible.</p>
                         <p>Disculpeu les mol&egrave;sties que aquesta aturada us pugui ocasionar.</p>
                     </div>
                 </div>
@@ -37,28 +38,37 @@
                         <p><br/><br/></p>
                         <p>L'espai al que est&agrave;s intentant accedir ha canviat d'adreça.</p>
                         <p>L'adreça nova &eacute;s:<br/> 
-                            <p style="text-align:center;"><a href="<?php echo $_GET['newaddress'] ?>">
-                                    <?php echo $_GET['newaddress'] ?></a></p>
+                            <p style="text-align:center;"><a href="<?php echo $_GET['newaddress'] ?>"><?php echo $_GET['newaddress'] ?></a></p>
                         </p>
                         <p><br/>Per aquest motiu, és recomanable que, tan aviat com puguis, actualitzis l'enllaç i, en cas que siguis l'administrador/a t'asseguris de que no hi ha cap enllaç trencat.</p> 
                     </div>
                 </div>	
-            <?php } else { ?>
-                <h1 class="titol imp">ACC&Eacute;S ERRONI A UN SERVEI D'&Agrave;GORA</h1>
-                <div class="content">
-                    <div>
-                        <p>No s'ha trobat l'espai al qual has intentat accedir. Les causes m&eacute;s probables s&oacute;n que hagis escrit
-                            l'adreça incorrecta a la finestra del navegador o que l'espai sol·licitat no estigui operatiu.</p>
-                        <p>L'adreça que has escrit ha estat <strong><?php echo $agora['server']['server'] . $agora['server']['base'] . $_GET['dns'] ?>/<?php echo $_GET['s'] ?></strong></p>
-                        <p>Si no ho heu fet encara, podeu sol·licitar l'alta als serveis d'&Agrave;gora des d'<a href="<?php echo $agora['server']['server'] . $agora['server']['base'] ?>portal/">aquí</a>.</p>
+            <?php } else {
+                $dns = $_GET['dns'];
+                if (!isValidDNS($dns)) {
+                    // El nom propi no és vàlid. No es pot mostrar per evitar problemes de XSS.
+                ?>
+                    <h1 class="titol imp">URL D'ÀGORA NO VÀLID</h1>
+                    <div class="content">
+                        <p>L'URL que heu indicat no es correspon amb cap URL vàlid del servei Àgora
+                           de la XTEC. Si us plau, reviseu-lo i torneu-ho a provar.</p>
                     </div>
-                    <div id="formulari">
-                        <div style="text-align: right;">
-                            <a href="<?php echo $agora['server']['server'] . $agora['server']['base'] ?>moodle/moodle/mod/resource/view.php?id=661">Condicions d'&uacute;s</a>
+                <?php } else { ?>
+                    <h1 class="titol imp">ACC&Eacute;S ERRONI A UN SERVEI D'&Agrave;GORA</h1>
+                    <div class="content">
+                        <div>
+                            <p>No s'ha trobat l'espai al qual has intentat accedir. Les causes m&eacute;s probables s&oacute;n que hagis escrit
+                                l'adreça incorrecta a la finestra del navegador o que l'espai sol·licitat no estigui operatiu.</p>
+                            <p>L'adreça que has escrit ha estat <strong><?php echo $agora['server']['server'] . $agora['server']['base'] . $dns . '/' . $_GET['s'] ?></strong></p>
+                            <p>Si no ho heu fet encara, podeu sol·licitar l'alta als serveis d'&Agrave;gora des d'<a href="<?php echo $agora['server']['server'] . $agora['server']['base'] ?>portal/">aquí</a>.</p>
+                        </div>
+                        <div id="formulari">
+                            <div style="text-align: right;">
+                                <a href="<?php echo $agora['server']['server'] . $agora['server']['base'] ?>moodle/moodle/mod/resource/view.php?id=661">Condicions d'&uacute;s</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            <?php } ?>
+            <?php } } ?>
 
             <div class="footer imp">
                 <p>&nbsp;</p>
