a5844f668754a49fb1d2970e84cea913bb06c1af
Albert <aperez16@xtec.cat>
2013-05-22 10:43:50 +0200

    Deleted module Downloads files, fixed problem in IWmoodle tables def

--- html/zikula2/modules/IWmoodle/tables.php
+++ html/zikula2/modules/IWmoodle/tables.php
@@ -20,58 +20,34 @@ function IWmoodle_tables() {
     // Initialise table array
     $table = array();
 
-    // IWmoodle table definition
+    // iw_webbox table definition
     $table['IWmoodle'] = DBUtil::getLimitedTablename('IWmoodle');
     $table['IWmoodle_column'] = array('mid' => 'iw_mid',
         'mcid' => 'iw_mcid',
         'uid' => 'iw_uid',
         'role' => 'iw_role');
 
-    $table['IWmoodle_column_def'] = array('mid' => "INT(11) NOTNULL AUTOINCREMENT KEY",
-        'mcid' => "INT(11) NOTNULL DEFAULT '0'",
-        'uid' => "INT(11) NOTNULL DEFAULT '0'",
-        'role' => "INT(11) NOTNULL DEFAULT '0'");
-
-    ObjectUtil::addStandardFieldsToTableDefinition($table['IWmoodle_column'], 'pn_');
-    ObjectUtil::addStandardFieldsToTableDataDefinition($table['IWmoodle_column_def'], 'iw_');
+    $table['IWmoodle_column_def'] = array('mid' => "I NOTNULL AUTO PRIMARY",
+        'mcid' => "I NOTNULL DEFAULT '0'",
+        'uid' => "I NOTNULL DEFAULT '0'",
+        'role' => "I NOTNULL DEFAULT '0'");
 
     /*
       // Define tables from groups and users that are used in this module
       $table['group_membership'] = DBUtil::getLimitedTablename('group_membership');
-      $table['group_membership_column'] = array('uid' => 'uid');
+      $table['group_membership_column'] = array('pn_uid' => 'pn_uid');
 
       $table['users'] = DBUtil::getLimitedTablename('users');
-      $table['users_column'] = array('uid' => 'uid',
-      'pass' => 'pass',
-      'uname' => 'uname');
-     * 
+      $table['users_column'] = array('pn_uid' => 'pn_uid',
+      'pn_pass' => 'pn_pass',
+      'pn_uname' => 'pn_uname');
      */
-/*
-    $table['users'] = 'users';
-    $table['users_column'] = array('uid' => 'uid',
-        'uname' => 'uname',
-        'pass' => 'pass',
-    );
 
-    $table['group_membership'] = 'group_membership';
-    $table['group_membership_column'] = array('gid' => 'gid',
-        'uid' => 'uid');
 
-    $table['IWusers'] = 'IWusers';
-    $table['IWusers_column'] = array('uid' => 'iw_uid',
-        'nom' => 'iw_nom',
-        'cognom1' => 'iw_cognom1',
-    );
-*/
-    /*
-      $items = array('uid' => $items['uid'],
-      'nom' => $nom,
-      'cognom1' => $cognoms);
-      if (!DBUtil::insertObject($items, 'IWusers', 'suid')) {
-      return LogUtil::registerError($this->__('Error! Creation attempt failed.'));
-      }
-     * 
-     */
+
+
+    ObjectUtil::addStandardFieldsToTableDefinition($table['IWmoodle_column'], 'pn_');
+    ObjectUtil::addStandardFieldsToTableDataDefinition($table['IWmoodle_column_def'], 'iw_');
 
     // Return the table information
     return $table;
