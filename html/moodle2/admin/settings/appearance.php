82b813718e63cb4b9f53dafbb146cfc6551fa28e
Sara Arjona <sarjona@xtec.cat>
2013-11-12 08:11:24 +0100

    Showed again Appearance | Default My home page parameters page

--- html/moodle2/admin/settings/appearance.php
+++ html/moodle2/admin/settings/appearance.php
@@ -171,16 +171,8 @@ if ($hassiteconfig) { // speedup for non-admins, add all caps used on this page
     $temp->add(new admin_setting_configcheckbox('doctonewwindow', new lang_string('doctonewwindow', 'admin'), new lang_string('configdoctonewwindow', 'admin'), 0));
     $ADMIN->add('appearance', $temp);
 
-    //XTEC ************ AFEGIT - To let access only to xtecadmin user
-    //2012.06.20  @sarjona
-    if (get_protected_agora() ) {
-    //************ FI    
     $temp = new admin_externalpage('mypage', new lang_string('mypage', 'admin'), $CFG->wwwroot . '/my/indexsys.php');
     $ADMIN->add('appearance', $temp);
-    //XTEC ************ AFEGIT - To let access only to xtecadmin user
-    //2012.06.20  @sarjona
-    }
-    //************ FI    
 
     $temp = new admin_externalpage('profilepage', new lang_string('myprofile', 'admin'), $CFG->wwwroot . '/user/profilesys.php');
     $ADMIN->add('appearance', $temp);
