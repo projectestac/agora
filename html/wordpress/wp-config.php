44475a7aa17eb9fe5d5c9f480a23d45b4ebd4d73
Toni Ginard <aginard@xtec.cat>
2014-07-18 16:55:18 +0200

    WordPress: Added basic statistics collection

--- html/wordpress/wp-config.php
+++ html/wordpress/wp-config.php
@@ -102,3 +102,6 @@ if ( !defined('ABSPATH') ) {
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+// Call to XTEC function to collect basic stats info
+save_stats();
--- html/wordpress/wp-includes/xtec/lib.php
+++ html/wordpress/wp-includes/xtec/lib.php
@@ -14,3 +14,62 @@ function is_xtecadmin() {
         return false;
     }
 }
+
+/*
+ * Collect basic statistical and security information.
+ */
+
+function save_stats() {
+
+    global $current_user, $table_prefix, $wpdb;
+
+    $table = $table_prefix . 'stats';
+
+    // time() return time referred to GMT. Changing the time zone fixes this.
+    date_default_timezone_set('Europe/Madrid');
+    $datetime = date('Y-m-d H:i:s', time());
+
+    $ip = $ipForward = $ipClient = $userAgent = '';
+
+    // Usage of filter_input() guarantees that info is clean
+    if (isset($_SERVER['REMOTE_ADDR']) && !empty($_SERVER['REMOTE_ADDR'])) {
+        $ip = filter_input(INPUT_SERVER, 'REMOTE_ADDR', FILTER_SANITIZE_STRING);
+    }
+
+    if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ipForward = filter_input(INPUT_SERVER, 'HTTP_X_FORWARDED_FOR', FILTER_SANITIZE_STRING);
+    }
+
+    if (isset($_SERVER['HTTP_CLIENT_IP']) && !empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ipClient = filter_input(INPUT_SERVER, 'HTTP_CLIENT_IP', FILTER_SANITIZE_STRING);
+    }
+
+    if (isset($_SERVER['HTTP_USER_AGENT']) && !empty($_SERVER['HTTP_USER_AGENT'])) {
+        $userAgent = filter_input(INPUT_SERVER, 'HTTP_USER_AGENT', FILTER_SANITIZE_STRING);
+    }
+
+    if (isset($_SERVER['REQUEST_URI']) && !empty($_SERVER['REQUEST_URI'])) {
+        $uri = filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_STRING);
+    }
+
+    $uid = $current_user->ID;
+    $username = $current_user->user_login;
+    $email = $current_user->user_email;
+
+    $isadmin = current_user_can('manage_options');
+
+    $data = array(
+        'datetime' => $datetime,
+        'ip' => $ip,
+        'ipForward' => $ipForward,
+        'ipClient' => $ipClient,
+        'userAgent' => $userAgent,
+        'uri' => $uri,
+        'uid' => $uid,
+        'isadmin' => $isadmin,
+        'username' => $username,
+        'email' => $email
+    );
+
+    $wpdb->insert($table, $data, array('%s', '%s', '%s', '%s', '%s', '%s', '%d', '%d', '%s', '%s'));
+}
