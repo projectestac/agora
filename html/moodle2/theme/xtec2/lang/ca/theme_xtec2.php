33db95a67a9221d273da6db0ce7120bd97cdeb1f
pau.ferrer-ocana <pau.ferrer-ocana@upcnet.es>
2014-08-14 13:11:18 +0200

    MOODLE2: Translated theme settings

---
+++ html/moodle2/theme/xtec2/lang/ca/theme_xtec2.php
@@ -0,0 +1,93 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_xtec2', language 'en'
+ *
+ * @package   theme_xtec2
+ * @copyright 2013 Moodle, moodle.org
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['choosereadme'] = 'Tema dissenyat pel servei Àgora-Moodle de la XTEC';
+
+$string['configtitle'] = 'Xtec2';
+
+$string['header_settings'] = 'Paràmetres de la capçalera';
+$string['logo'] = 'Imatge de la capçalera';
+$string['logodesc'] = 'Alçada recomenada: 93px';
+$string['logo_color'] = 'Color del tint per a la imatge de la capçalera';
+$string['logo_color_transparency'] = 'Transparència del tint';
+$string['top_menus'] = 'Mostra el menú principal a la barra de navegació';
+$string['top_menus_description'] = 'Mostra els menús de Navegació i Configuració a la barra de navegació (experimental)';
+
+$string['disabled'] = 'Desactivat';
+
+$string['layout_settings'] = 'Paàmetres de disposició';
+$string['footnote'] = 'Nota a peu de pàgina';
+$string['footnotedesc'] = 'Qualsevol cosa que escriviu en aquesta àrea de text es mostrarà al peu de pàgina al vostre lloc Moodle.';
+$string['block_layout'] = 'Disposició dels blocs';
+$string['without_blocks'] = 'Sense blocs';
+$string['blocks_left'] = 'Tots els blocks a l\'esquerra';
+$string['blocks_right'] = 'Tots els blocks a la dreat';
+$string['blocks_both'] = 'Blocs als dos laterals (No aplica a tot el lloc)';
+
+$string['color_settings'] = 'Paràmetres de color';
+$string['colorset'] = 'Combinació de colors';
+$string['colorsetdesc'] = 'Trieu la combinació de colors que utilitzarà el tema. Podeu modificar qualsevol dels colors de les combinacions predefinides.';
+
+$string['custom'] = 'Personalitzat'; //Personalitzat
+$string['grana'] = 'Grana (predefinit)'; //'Grana (predefinit)'
+$string['coral'] = 'Escull de coral';//'Escull de coral'
+$string['or'] = 'Febre de l\'or'; //'Febre de l\'or'
+$string['llima'] = 'Llima dolça'; //'Llima dolça'
+$string['tardor'] = 'Tardor'; //'Tardor'
+$string['nostalgia'] = 'Nostàlgia'; //'Nostàlgia'
+
+
+$string['color2'] = 'Fons de les capçaleres dels blocs';
+$string['color3'] = 'Color de la lletra de les capçaleres dels blocs';
+$string['color4'] = 'Color de la lletra dels títols';
+$string['color5'] = 'Color de la lletra dels enllaços';
+
+$string['font_settings'] = 'Paràmetres de font';
+$string['fontsize'] = 'Mida de la lletra';
+$string['fontsizedesc'] = 'Trieu la mida de la lletra que s\'utilitzarà com a referència. Totes les mides de lletra del tema són relatives a aquesta selecció';
+$string['fontsizedesc1'] = 'Molt petita';
+$string['fontsizedesc2'] = 'Petita';
+$string['fontsizedesc3'] = 'Mitjana';
+$string['fontsizedesc4'] = 'Gran';
+$string['fontsizedesc5'] = 'Molt gran';
+
+$string['fontstyle'] = 'Estil de la lletra';
+$string['fontstyledesc'] = 'Trieu l\'aspecte que han de tenir les lletres';
+$string['fontstyledesc1'] = 'Lletra normal';
+$string['fontstyledesc2'] = 'Lletra lligada';
+$string['fontstyledesc3'] = 'Lletra en majúscules';
+
+
+$string['css_settings'] = 'Paràmetres CSS';
+$string['importcss'] = 'Full d\'estils extern';
+$string['importcssdesc'] = 'Introduïu un URL absolut d\'un fitxer de fulls d\'estils CSS. Es carregarà abans dels estils del tema';
+
+$string['customcss'] = 'Estils personalitzats';
+$string['customcssdesc'] = 'Les regles definides aquí seran les darreres que s\'aplicaran i, per tant, els seus valors haurien de prevaldre per sobre dels que puguin estar definits prèviament. Si no és així, reviseu la sintaxi o proveu afegir <code>!important</code> a la regla.';
+
+$string['pluginname'] = 'XTEC2';
+
+$string['region-side-post'] = 'Dreta';
+$string['region-side-pre'] = 'Esquerra';
+
---
+++ html/moodle2/theme/xtec2/lang/es/theme_xtec2.php
@@ -0,0 +1,92 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_xtec2', language 'en'
+ *
+ * @package   theme_xtec2
+ * @copyright 2013 Moodle, moodle.org
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['choosereadme'] = 'Tema dissenyat pel servei Àgora-Moodle de la XTEC';
+
+$string['configtitle'] = 'Xtec2';
+
+$string['header_settings'] = 'Parámetros de la cabecera';
+$string['logo'] = 'Imagen de la cabecera';
+$string['logodesc'] = 'Altura recomendada: 93px';
+$string['logo_color'] = 'Color del tinte para la imagen de la cabecera';
+$string['logo_color_transparency'] = 'Transparencia del tinte';
+$string['top_menus'] = 'Mostrar el menú principal en la barra de navegación';
+$string['top_menus_description'] = 'Mostrar los menús de Navegación y Parámetros en la barra de navegación (experimental)';
+
+$string['disabled'] = 'Desactivado';
+
+$string['layout_settings'] = 'Parámetros de disposición';
+$string['footnote'] = 'Nota a pie de página';
+$string['footnotedesc'] = 'Lo que añada en esta área de texto se mostrará en el pie de página de todo su sitio Moodle.';
+$string['block_layout'] = 'Disposición de los bloques';
+$string['without_blocks'] = 'Sin bloques';
+$string['blocks_left'] = 'Todos los bloques a la izquierda';
+$string['blocks_right'] = 'Todos los bloques a la derecha';
+$string['blocks_both'] = 'Bloques en los dos laterales (no aplica a ltodo el sitio)';
+
+$string['color_settings'] = 'Parámetros de color';
+$string['colorset'] = 'Combinación de colores';
+$string['colorsetdesc'] = 'Elije la combinación de colores para usar en el tema. Se pueden modificar los predefinidos.';
+
+$string['custom'] = 'Personalizado'; //Personalitzat
+$string['grana'] = 'Granate (predefinido)'; //'Grana (predefinit)'
+$string['coral'] = 'Arrecife de coral';//'Escull de coral'
+$string['or'] = 'Fiebre del oro'; //'Febre de l\'or'
+$string['llima'] = 'Lima fresca'; //'Llima dolça'
+$string['tardor'] = 'Otoño'; //'Tardor'
+$string['nostalgia'] = 'Nostálgia'; //'Nostàlgia'
+
+
+$string['color2'] = 'Fondo de la cabecera de los bloques';
+$string['color3'] = 'Color de la fuente de la cabecera de los bloques';
+$string['color4'] = 'Color de la fuente delos títulos';
+$string['color5'] = 'Color de los enlaces';
+
+$string['font_settings'] = 'Parámetros de fuente';
+$string['fontsize'] = 'Tamaño de la fuente';
+$string['fontsizedesc'] = 'Elije un tamaño para usarlo como referencia. Todos los tamaños seran relativos a éste.';
+$string['fontsizedesc1'] = 'Muy pequeña';
+$string['fontsizedesc2'] = 'Pequeña';
+$string['fontsizedesc3'] = 'Mediana';
+$string['fontsizedesc4'] = 'Grande';
+$string['fontsizedesc5'] = 'Muy grande';
+
+$string['fontstyle'] = 'Estilo de fuente';
+$string['fontstyledesc'] = 'Elije una familia de fuente a usar';
+$string['fontstyledesc1'] = 'Fuente normal';
+$string['fontstyledesc2'] = 'Fuente redondilla';
+$string['fontstyledesc3'] = 'Fuente en mayúsculas';
+
+
+$string['css_settings'] = 'Parámetros CSS';
+$string['importcss'] = 'Importar CSS externo';
+$string['importcssdesc'] = 'Introduce una URL absoluta hacia el archivo CSS. Se cargará antes que todos los estilos del tema.';
+
+$string['customcss'] = 'CSS personalizado';
+$string['customcssdesc'] = 'Todas las reglas CSS que añada a esta área de texto se reflejarán en todas las páginas, lo que facilita la personalización de este tema.';
+
+$string['pluginname'] = 'XTEC2';
+
+$string['region-side-post'] = 'Derecha';
+$string['region-side-pre'] = 'Izquierda';
