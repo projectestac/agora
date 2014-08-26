eae254918ca15b61f24acd5c5f591c6a27e5773d
Pau Ferrer Ocaña <crazyserver@gmail.com>
2014-08-26 09:52:32 +0200

    MOODLE2: Only le configure Google Oauth settings

--- html/moodle2/auth/googleoauth2/auth.php
+++ html/moodle2/auth/googleoauth2/auth.php
@@ -508,8 +508,8 @@ class auth_plugin_googleoauth2 extends auth_plugin_base {
 
         //XTEC ************ ELIMINAT - Only let configure Google OAuth
         //2014.08.15  @pferre22
+        if(is_xtecadmin()){
         // Facebook client id
-        /*
         echo '<tr>
                 <td align="right"><label for="facebookclientid">';
 
@@ -730,7 +730,7 @@ class auth_plugin_googleoauth2 extends auth_plugin_base {
         print_string('auth_googleipinfodbkey', 'auth_googleoauth2', (object) array('website' => $CFG->wwwroot)) ;
 
         echo '</td></tr>';
-        */
+        }
         //************ FI
         // User prefix
 
