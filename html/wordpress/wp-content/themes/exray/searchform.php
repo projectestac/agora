6bd8640f915cb5005df15e907195aa37c948abd9
Sara Arjona <sarjona@xtec.cat>
2014-08-29 10:11:28 +0200

    Removed exray theme

--- html/wordpress/wp-content/themes/exray/searchform.php
+++
@@ -1,7 +0,0 @@
-<form action="<?php echo esc_url(home_url()); ?>" id="search-form" method="get">
-
-	<input type="text" value="<?php echo esc_attr( 'Search' ); ?>" name="s" id="s" onblur="if(this.value=='')this.value='search'" 
-		onfocus="if(this.value=='search') this.value=''" />
-	<input type="hidden" value="submit" />
-
-</form>
