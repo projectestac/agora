676af2bdfed6c8a84755a5957daf86eeb3c6e99d
pau.ferrer-ocana <pau.ferrer-ocana@upcnet.es>
2014-09-04 12:58:38 +0200

    MOODLE2: Google Oauth Translated

---
+++ html/moodle2/lang/ca/auth_googleoauth2.php
@@ -0,0 +1,41 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'auth_googleoauth2', language 'ca', branch 'MOODLE_25_STABLE'
+ *
+ * @package   auth_googleoauth2
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['auth_facebookclientid'] = 'L\'ID de l\'aplicació - es pot trobar a https://developers.facebook.com/apps/.';
+$string['auth_facebookclientid_key'] = 'ID de l\'aplicació de Facebook';
+$string['auth_facebookclientsecret'] = 'La vostra clau secreta de l\'aplicació - es pot trobar a https://developers.facebook.com/apps/.';
+$string['auth_facebookclientsecret_key'] = 'Clau secreta de l\'aplicació de Facebook';
+$string['auth_googleclientid'] = 'El vostre ID de client - es pot trobar a la Google console API.';
+$string['auth_googleclientid_key'] = 'ID de client de Google';
+$string['auth_googleclientsecret'] = 'El vostre secret de client - es pot trobar a la Google console API.';
+$string['auth_googleclientsecret_key'] = 'Secret de client de Google';
+$string['auth_googlesettings'] = 'Configuració';
+$string['couldnotgetgoogleaccesstoken'] = 'El proveïdor de l\'autenticació ha retornat un error de comunicació. Torneu a intentar-ho de nou.';
+$string['emailaddressmustbeverified'] = 'La vostra adreça de correu electrònic no s\'ha verificat pel mètode d\'autenticació que havíeu seleccionat. Segurament heu oblidat fer clic a l\'enllaç "verify email address" que Google o Facebook.';
+$string['moreproviderlink'] = 'Registreu-vos amb un altre servei.';
+$string['noaccountyet'] = 'No teniu permís per fer servir aquest lloc. Contacteu amb el vostre administrador/a i pregunteu com activar el vostre compte.';
+$string['signinwithanaccount'] = 'Entra amb:';
