926bffb48551fec17ffa60b379c67a628b29780c
Toni Ginard <toni.ginard@gmail.com>
2014-09-16 17:16:25 +0200

    WORDPRESS: Moved Social Articles and Invite Anyone to custom menu page

--- html/wordpress/wp-content/themes/reactor-primaria-1/functions.php
+++ html/wordpress/wp-content/themes/reactor-primaria-1/functions.php
@@ -591,6 +591,7 @@ function bp_options_page() {
             <p><a href="admin.php?page=bp-activity"><?php _e('Activity', 'buddypress'); ?></a></p>
             <p><a href="admin.php?page=bpfb-settings"><?php _e('Activity Plus', 'bpfb'); ?></a></p>
             <p><a href="admin.php?page=bp-groups"><?php _e('Groups', 'buddypress'); ?></a></p>
+            <p><a href="admin.php?page=social-articles"><?php _e('Social Articles', 'social-articles'); ?></a></p>
             <p><a href="admin.php?page=bp-like-settings"><?php _e('BuddyPress Like', 'buddypress-like'); ?></a></p>
             <p><a href="admin.php?page=ass_admin_options"><?php _e('Group Email Options', 'bp-ass'); ?></a></p>
         </div>
@@ -606,6 +607,7 @@ function bp_options_page() {
 
         <div style="width:150px; padding:20px; float:left;">
             <h3 style="height:40px;"><?php _e('BuddyPress Invitations', 'bp-invite-anyone'); ?></h3>
+            <p><a href="admin.php?page=invite-anyone"><?php _e('Invite Anyone', 'bp-invite-anyone'); ?></a></p>
             <p><a href="edit.php?post_type=ia_invites"><?php _e('BuddyPress Invitations', 'bp-invite-anyone'); ?></a></p>
             <p><a href="post-new.php?post_type=ia_invites"><?php _e('Add New', 'bp-invite-anyone'); ?></a></p>
             <p><a href="edit-tags.php?taxonomy=ia_invitees&post_type=ia_invites"><?php _e('Invitee', 'bp-invite-anyone'); ?></a></p>
@@ -634,6 +636,7 @@ function bp_options_page() {
 function rebuild_bp_menus_step_1() {
 
     remove_submenu_page('options-general.php', 'bpfb-settings'); // Activity Plus
+    remove_submenu_page('options-general.php', 'invite-anyone'); // Invite anyone
 
     add_menu_page(__('BuddyPress', 'buddypress'), __('BuddyPress', 'buddypress'), 'manage_options', 'xtec-bp-options', 'bp_options_page', '', 59);
 
@@ -641,7 +644,10 @@ function rebuild_bp_menus_step_1() {
     add_submenu_page('xtec-bp-options', __('Activity', 'buddypress'), __('Activity', 'buddypress'), 'manage_options', 'bp-activity');
     add_submenu_page('xtec-bp-options', __('Activity Plus', 'bpfb'), __('Activity Plus', 'bpfb'), 'manage_options', 'bpfb-settings', 'settings_page');
     add_submenu_page('xtec-bp-options', __('Groups', 'buddypress'), __('Groups', 'buddypress'), 'manage_options', 'bp-groups');
+    add_submenu_page('xtec-bp-options', __('Social Articles', 'social-articles'), __('Social Articles', 'social-articles'), 'manage_options', 'social-articles', 'social_articles_page');
+    add_submenu_page('xtec-bp-options', __('BuddyPress Like', 'buddypress-like'), __('BuddyPress Like', 'buddypress-like'), 'manage_options' , 'bp-like-settings' , 'bp_like_admin_page');
 	add_submenu_page('xtec-bp-options', __('Group Email Options', 'bp-ass'), __('Group Email Options', 'bp-ass'), 'manage_options', 'ass_admin_options', 'ass_admin_options');
+	add_submenu_page('xtec-bp-options', __('Invite Anyone', 'invite-anyone'), __('Invite Anyone', 'invite-anyone'), 'manage_options', 'invite-anyone', 'invite_anyone_admin_panel');
 
 }
 
@@ -661,8 +667,7 @@ function rebuild_bp_menus_step_2() {
     remove_submenu_page('options-general.php', 'bp-components'); // Tab in BuddyPress
     remove_submenu_page('bp-general-settings', 'ass_admin_options'); // Group Email 
     remove_submenu_page('options-general.php', 'bp-like-settings'); // BuddyPress Like
-
-    add_submenu_page('xtec-bp-options', __('BuddyPress Like', 'buddypress-like'), __('BuddyPress Like', 'buddypress-like'), 'manage_options' , 'bp-like-settings' , 'bp_like_admin_page');
+    remove_submenu_page('options-general.php', 'social-articles'); // Social Articles
 
 }
 
