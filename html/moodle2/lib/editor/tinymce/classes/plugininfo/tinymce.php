e22e32fa10f548b06041b5da172c77afbd7ebd13
Pau Ferrer Ocaña <crazyserver@gmail.com>
2014-09-19 11:26:43 +0200

    MOODLE2: Hide unsintall method for tinymce buttons

--- html/moodle2/lib/editor/tinymce/classes/plugininfo/tinymce.php
+++ html/moodle2/lib/editor/tinymce/classes/plugininfo/tinymce.php
@@ -59,6 +59,12 @@ class tinymce extends base {
     }
 
     public function is_uninstall_allowed() {
+        //XTEC ************ AFEGIT - Disable uninstalling
+        //2014.09.09  @pferre22
+        if (!get_protected_agora()) {
+            return false;
+        }
+        //************ FI
         return true;
     }
 
