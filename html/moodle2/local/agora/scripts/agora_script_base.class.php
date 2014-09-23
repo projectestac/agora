3b17b654364bf93385d17177f95591239e9af19c
Pau Ferrer Ocaña <crazyserver@gmail.com>
2014-09-23 11:17:12 +0200

    SOolved xtecadmin problem with Agora scripts

--- html/moodle2/local/agora/scripts/agora_script_base.class.php
+++ html/moodle2/local/agora/scripts/agora_script_base.class.php
@@ -7,44 +7,44 @@ class agora_script_base{
 	public $cron = false;
 	protected $test = true;
 
-	protected function params(){
+	protected function params() {
 		$params = array();
 		return $params;
 	}
 
-	function execute_web($action){
+	function execute_web($action) {
 		global $OUTPUT;
-		echo $OUTPUT->heading($this->title,3);
+		echo $OUTPUT->heading($this->title, 3);
 		echo $OUTPUT->box($this->info);
 
 
 		$params = $this->params();
 		$this->form($params);
-		if($action == 'test'){
+		if ($action == 'test') {
 			echo $OUTPUT->notification('Testing...');
 			return $this->_execute($params, false);
-		} else if($action == 'execute' && $this->can_be_executed($params)){
+		} else if ($action == 'execute' && $this->can_be_executed($params)) {
 			echo $OUTPUT->notification('Executing!!');
 			return $this->_execute($params);
 		}
 		return false;
 	}
 
-	function execute($params){
-		if($this->can_be_executed($params)){
+	function execute($params) {
+		if ($this->can_be_executed($params)) {
 			return $this->_execute($params);
 		}
 		return false;
 	}
 
-	protected function _execute($params = array(), $execute = true){
+	protected function _execute($params = array(), $execute = true) {
 		return false;
 	}
 
-	function form($params){
+	function form($params) {
 		echo '<form class="mform" method="post" action="?script='.get_class($this).'">';
 		$more_params = $this->_form($params);
-		foreach($more_params as $paramname => $value){
+		foreach ($more_params as $paramname => $value) {
 			echo '<div class="fitem fitem_ftext ">';
 			echo '<div class="fitemtitle"><label for="'.$paramname.'">'.$paramname.'</label></div>';
 			echo '<div class="felement ftext"><input name="'.$paramname.'" id="'.$paramname.'" value="'.$value.'"/></div>';
@@ -52,27 +52,27 @@ class agora_script_base{
 		}
 		echo '<div id="fgroup_id_buttonar" class="fitem fitem_actionbuttons fitem_fgroup"><div class="felement fgroup">';
 		echo '<input type="submit" name="action" value="execute" id="id_submitbutton">';
-		if($this->test){
+		if ($this->test) {
 			echo '<input type="submit" name="action" value="test">';
 		}
 		echo '</div></div>';
 		echo '</form>';
 	}
 
-	protected function _form($params){
+	protected function _form($params) {
 		return $params;
 	}
 
 	/**
 	 * Function to be executed when cron runs
 	 */
-	function cron(){
-		if($this->cron && $this->can_be_executed()){
+	function cron() {
+		if ($this->cron && $this->can_be_executed()) {
 			mtrace('Script: '.$this->title);
 
 			$success = $this->_cron();
 
-			if($success){
+			if ($success) {
 				mtrace('Done');
 			} else {
 				mtrace('Failed');
@@ -83,16 +83,22 @@ class agora_script_base{
 		return true;
 	}
 
-	protected function _cron(){
+	protected function _cron() {
 		return true;
 	}
 
-	protected function can_be_executed($params = array()){
+	protected function can_be_executed($params = array()) {
 		return true;
 	}
 
-	function is_visible(){
-		return is_xtecadmin();
+	function is_visible() {
+		if (is_agora() && !is_xtecadmin()) {
+			return false;
+        }
+        if (!is_siteadmin()) {
+            return false;
+        }
+        return true;
 	}
 
 }
