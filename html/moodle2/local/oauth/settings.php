fb845b20c73d94afed4be9f28cb2bf96f641569a
Pau Ferrer Ocaña <crazyserver@gmail.com>
2014-10-03 10:59:51 +0200

    MOODLE2: Changed Oauth server params to Server menu

--- html/moodle2/local/oauth/settings.php
+++ html/moodle2/local/oauth/settings.php
@@ -1,5 +1,5 @@
 <?php
 if ($hassiteconfig) {
 	//$ADMIN->add('root', new admin_category('local_oauth', get_string('pluginname','local_oauth')));
-	$ADMIN->add('root', new admin_externalpage('local_oauth_settings', get_string('settings','local_oauth'), $CFG->wwwroot . '/local/oauth/index.php', array('local/oauth:manageclients')));
+	$ADMIN->add('server', new admin_externalpage('local_oauth_settings', get_string('settings','local_oauth'), $CFG->wwwroot . '/local/oauth/index.php', array('local/oauth:manageclients')));
 }
