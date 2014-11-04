cf2d2c184e492a9673db4d8d8f3b936b218091f6
Toni Ginard <toni.ginard@gmail.com>
2014-11-04 16:38:36 +0100

    NODES: BP-like: Fixed loading gettext language files

--- html/wordpress/wp-content/plugins/buddypress-like/includes/bplike.php
+++ html/wordpress/wp-content/plugins/buddypress-like/includes/bplike.php
@@ -16,7 +16,18 @@ if ( !defined( 'BPLIKE_PATH' ) ) {
     define( 'BPLIKE_PATH' , plugin_dir_path( dirname( __FILE__ ) ) );
 }
 
+
+// XTEC ************ MODIFICAT - Fixed loading gettext language files
+// 2014.11.04 @aginard
+
+load_plugin_textdomain( 'buddypress-like' , false , 'buddypress-like/languages/' );
+
+//************ ORIGINAL
+/*
 load_plugin_textdomain( 'buddypress-like' , false , BPLIKE_PATH . '/languages/' );
+*/
+//************ FI
+
 
 /**
  * bp_like_get_text()
