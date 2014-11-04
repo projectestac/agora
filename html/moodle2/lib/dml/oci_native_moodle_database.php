13f7c60107985b8d15753de6f91fca62477f8354
Pau Ferrer Ocaña <crazyserver@gmail.com>
2014-11-04 18:05:53 +0100

    MOODLE2: Fixed linguistic SQL sorting

--- html/moodle2/lib/dml/oci_native_moodle_database.php
+++ html/moodle2/lib/dml/oci_native_moodle_database.php
@@ -219,6 +219,13 @@ class oci_native_moodle_database extends moodle_database {
         // Connection stabilised and configured, going to instantiate the temptables controller
         $this->temptables = new oci_native_moodle_temptables($this, $this->unique_session_id);
 
+        //XTEC ************ AFEGIT - Fix for correct sorting
+        //2014.11.04 @pferre22
+        $sql = "ALTER session SET NLS_COMP = LINGUISTIC";
+        $this->execute($sql);
+        $sql = "ALTER session SET NLS_SORT = BINARY_AI";
+        $this->execute($sql);
+        //************ FI
         return true;
     }
 
