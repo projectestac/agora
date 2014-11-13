39c27f41948c04675b81ccfe04186f52f231ddd6
pau.ferrer-ocana <pau.ferrer-ocana@upcnet.es>
2014-11-13 12:54:25 +0100

    MOODLE2: cacheconfig without memcache

---
+++ html/moodle2/local/agora/muc/cacheconfig-form.php
@@ -0,0 +1,377 @@
+<?php
+global $CFG;
+defined('MOODLE_INTERNAL') || die();
+ $configuration = array (
+  'siteidentifier' => 'unknown',
+  'stores' =>
+  array (
+    'default_application' =>
+    array (
+      'name' => 'default_application',
+      'plugin' => 'file',
+      'configuration' =>
+      array (
+        'path' =>  $CFG->agora_muc_path,
+        'autocreate' => true
+      ),
+      'features' => 14,
+      'modes' => 3,
+      'default' => true,
+      'class' => 'cachestore_file',
+      'lock' => 'cachelock_file_default',
+    ),
+    'default_session' =>
+    array (
+      'name' => 'default_session',
+      'plugin' => 'session',
+      'configuration' =>
+      array (
+      ),
+      'features' => 14,
+      'modes' => 2,
+      'default' => true,
+      'class' => 'cachestore_session',
+      'lock' => 'cachelock_file_default',
+    ),
+    'default_request' =>
+    array (
+      'name' => 'default_request',
+      'plugin' => 'static',
+      'configuration' =>
+      array (
+      ),
+      'features' => 14,
+      'modes' => 4,
+      'default' => true,
+      'class' => 'cachestore_static',
+      'lock' => 'cachelock_file_default',
+    )
+  ),
+  'modemappings' =>
+  array (
+    0 =>
+    array (
+      'store' => 'default_request',
+      'mode' => 4,
+      'sort' => 0,
+    ),
+    1 =>
+    array (
+      'store' => 'default_session',
+      'mode' => 2,
+      'sort' => 0,
+    ),
+    2 =>
+    array (
+      'store' => 'default_application',
+      'mode' => 1,
+      'sort' => 0,
+    ),
+  ),
+  'definitions' =>
+  array (
+    'core/string' =>
+    array (
+      'mode' => 1,
+      'simplekeys' => true,
+      'simpledata' => true,
+      'staticacceleration' => true,
+      'staticaccelerationsize' => 30,
+      'component' => 'core',
+      'area' => 'string',
+      'selectedsharingoption' => 1,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/langmenu' =>
+    array (
+      'mode' => 1,
+      'simplekeys' => true,
+      'simpledata' => true,
+      'staticacceleration' => true,
+      'component' => 'core',
+      'area' => 'langmenu',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/databasemeta' =>
+    array (
+      'mode' => 1,
+      'requireidentifiers' =>
+      array (
+        0 => 'dbfamily',
+      ),
+      'staticacceleration' => true,
+      'staticaccelerationsize' => 15,
+      'component' => 'core',
+      'area' => 'databasemeta',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/eventinvalidation' =>
+    array (
+      'mode' => 1,
+      'staticacceleration' => true,
+      'requiredataguarantee' => true,
+      'simpledata' => true,
+      'component' => 'core',
+      'area' => 'eventinvalidation',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/questiondata' =>
+    array (
+      'mode' => 1,
+      'simplekeys' => true,
+      'requiredataguarantee' => false,
+      'datasource' => 'question_finder',
+      'datasourcefile' => 'question/engine/bank.php',
+      'component' => 'core',
+      'area' => 'questiondata',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/htmlpurifier' =>
+    array (
+      'mode' => 1,
+      'component' => 'core',
+      'area' => 'htmlpurifier',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/config' =>
+    array (
+      'mode' => 1,
+      'staticacceleration' => true,
+      'simpledata' => true,
+      'component' => 'core',
+      'area' => 'config',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/groupdata' =>
+    array (
+      'mode' => 1,
+      'simplekeys' => true,
+      'simpledata' => true,
+      'staticacceleration' => true,
+      'staticaccelerationsize' => 2,
+      'component' => 'core',
+      'area' => 'groupdata',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/calendar_subscriptions' =>
+    array (
+      'mode' => 1,
+      'simplekeys' => true,
+      'simpledata' => true,
+      'staticacceleration' => true,
+      'component' => 'core',
+      'area' => 'calendar_subscriptions',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/yuimodules' =>
+    array (
+      'mode' => 1,
+      'component' => 'core',
+      'area' => 'yuimodules',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/observers' =>
+    array (
+      'mode' => 1,
+      'simplekeys' => true,
+      'simpledata' => true,
+      'staticacceleration' => true,
+      'staticaccelerationsize' => 2,
+      'component' => 'core',
+      'area' => 'observers',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/plugin_manager' =>
+    array (
+      'mode' => 1,
+      'simplekeys' => true,
+      'simpledata' => true,
+      'component' => 'core',
+      'area' => 'plugin_manager',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/coursecattree' =>
+    array (
+      'mode' => 1,
+      'staticacceleration' => true,
+      'invalidationevents' =>
+      array (
+        0 => 'changesincoursecat',
+      ),
+      'component' => 'core',
+      'area' => 'coursecattree',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/coursecat' =>
+      array (
+      'mode' => 2,
+      'invalidationevents' =>
+      array (
+        0 => 'changesincoursecat',
+        1 => 'changesincourse',
+      ),
+      'ttl' => 600,
+      'component' => 'core',
+      'area' => 'coursecat',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 2,
+    ),
+    'core/coursecatrecords' =>
+    array (
+      'mode' => 4,
+      'simplekeys' => true,
+      'invalidationevents' =>
+      array (
+        0 => 'changesincoursecat',
+      ),
+      'component' => 'core',
+      'area' => 'coursecatrecords',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 2,
+    ),
+    'core/coursecontacts' =>
+    array (
+      'mode' => 1,
+      'staticacceleration' => true,
+      'simplekeys' => true,
+      'component' => 'core',
+      'area' => 'coursecontacts',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/repositories' =>
+    array (
+      'mode' => 4,
+      'component' => 'core',
+      'area' => 'repositories',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 2,
+    ),
+    'core/externalbadges' =>
+    array (
+      'mode' => 1,
+      'simplekeys' => true,
+      'ttl' => 3600,
+      'component' => 'core',
+      'area' => 'externalbadges',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/coursemodinfo' =>
+    array (
+      'mode' => 1,
+      'simplekeys' => true,
+      'component' => 'core',
+      'area' => 'coursemodinfo',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/userselections' =>
+    array (
+      'mode' => 2,
+      'simplekeys' => true,
+      'simpledata' => true,
+      'component' => 'core',
+      'area' => 'userselections',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 2,
+    ),
+    'core/gradecondition' =>
+    array (
+      'mode' => 1,
+      'staticacceleration' => true,
+      'staticaccelerationsize' => 2,
+      'ttl' => 3600,
+      'component' => 'core',
+      'area' => 'gradecondition',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 15,
+    ),
+    'core/navigation_expandcourse' =>
+    array (
+      'mode' => 2,
+      'simplekeys' => true,
+      'simpledata' => true,
+      'component' => 'core',
+      'area' => 'navigation_expandcourse',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 2,
+    ),
+    'core/suspended_userids' =>
+    array (
+      'mode' => 4,
+      'simplekeys' => true,
+      'simpledata' => true,
+      'component' => 'core',
+      'area' => 'suspended_userids',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 2,
+    ),
+    'repository_skydrive/foldername' =>
+    array (
+      'mode' => 2,
+      'component' => 'repository_skydrive',
+      'area' => 'foldername',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 2,
+    ),
+    'tool_uploadcourse/helper' =>
+    array (
+      'mode' => 4,
+      'component' => 'tool_uploadcourse',
+      'area' => 'helper',
+      'selectedsharingoption' => 2,
+      'userinputsharingkey' => '',
+      'sharingoptions' => 2,
+    ),
+  ),
+  'definitionmappings' =>
+  array (
+  ),
+  'locks' =>
+  array (
+    'cachelock_file_default' =>
+    array (
+      'name' => 'cachelock_file_default',
+      'type' => 'cachelock_file',
+      'dir' => 'filelocks',
+      'default' => true,
+    ),
+  ),
+);
