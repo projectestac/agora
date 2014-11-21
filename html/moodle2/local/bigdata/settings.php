a7f5a2e6e57eb743f6dea147b9610d5b64b41aab
Pau Ferrer Ocaña <crazyserver@gmail.com>
2014-11-21 14:42:29 +0100

    MOODLE2: Bigdata: Do not show option for non xtecadmin users

--- html/moodle2/local/bigdata/settings.php
+++ html/moodle2/local/bigdata/settings.php
@@ -2,16 +2,17 @@
 
 if ($hassiteconfig) {
     $isenabled = get_config('local_bigdata', 'enabled');
-    if ($isenabled) {
+    if ($isenabled && is_xtecadmin()) {
         $ADMIN->add('root', new admin_externalpage('bigdata', get_string('pluginname', 'local_bigdata'), $CFG->wwwroot . '/local/bigdata/index.php'));
     }
 
 }
 
-$settings = new admin_settingpage('local_bigdata', get_string('pluginname', 'local_bigdata'));
+if (is_xtecadmin()) {
+	$settings = new admin_settingpage('local_bigdata', get_string('pluginname', 'local_bigdata'));
     $ADMIN->add('localplugins', $settings);
 
-if (is_xtecadmin()) {
+
     $checkedyesno = array('0' => get_string('no'), '1' => get_string('yes')); // not nice at all
     $settings->add(new admin_setting_configselect('local_bigdata/enabled', get_string('enabled', 'local_bigdata'),
             get_string('enabled_desc', 'local_bigdata'), '', $checkedyesno));
