14930e9dc5c7fee4c37885b4200c5e55fb43611d
Toni Ginard <toni.ginard@gmail.com>
2015-01-21 12:30:23 +0100

    ZIKULA & PORTAL: Added ability to return to original URL when there is a redirect for login

--- html/zikula2/index.php
+++ html/zikula2/index.php
@@ -96,7 +96,7 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
 
 if (($module == 'users' || $module == 'usuaris') && $type == 'user' && ($func == 'login' || $func == 'loginscreen')) {
     if ($rurl == 'http') {
-        header('location:https://' . System::serverGetVar('HTTP_HOST') . System::getBaseUri() . '/' . ModUtil::url('users', 'user', 'login'));
+        header('location:https://' . System::serverGetVar('HTTP_HOST') . System::getBaseUri() . '/' . ModUtil::url('users', 'user', 'login') . '&returnpage=' . System::getCurrentUrl());
     }
 } else {
     if ($rurl == 'https') {
