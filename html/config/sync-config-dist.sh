4fa595cd86b7395b82ba13463bed4242b3a0b4da
Toni Ginard <toni.ginard@gmail.com>
2015-07-30 14:02:29 +0200

    Added info for FMO servers

--- html/config/sync-config-dist.sh
+++ html/config/sync-config-dist.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+# DEV
 PHP_PATH=/usr/bin/php
 
-#//------------ CMO
-<<COMENTAT
-PHP_PATH="/opt/xtk/php5/bin/php -c /opt/xtk/php5/lib/php.ini"
-COMENTAT
+# CMO
+#PHP_PATH="/opt/xtk/php5/bin/php -c /opt/xtk/php5/lib/php.ini"
+
+# FMO AGORA INT
+#PHP_PATH="/opt/rh/php54/root/usr/bin/php -c /serveis/conf/int/1051/apache/php.ini"
+
+# FMO AGORA PRE
+#PHP_PATH="/opt/rh/php54/root/usr/bin/php -c /serveis/conf/pre/1051/apache/php.ini"
+
+# FMO AGORA PRO
+#PHP_PATH="/opt/rh/php54/root/usr/bin/php -c /serveis/conf/pro/1051/apache/php.ini"
+
+# FMO AGORA-EOI INT
+#PHP_PATH="/opt/rh/php54/root/usr/bin/php -c /serveis/conf/int/1143/apache/php.ini"
+
+# FMO AGORA-EOI PRE
+#PHP_PATH="/opt/rh/php54/root/usr/bin/php -c /serveis/conf/pre/1143/apache/php.ini"
+
+# FMO AGORA-EOI PRO
+#PHP_PATH="/opt/rh/php54/root/usr/bin/php -c /serveis/conf/pro/11143/apache/php.ini"
