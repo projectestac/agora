ee6e038e4be3644c1d99d7e78c1eed92cb359795
toni.ginard <toni.ginard@ithinkupc.com>
2019-06-20 17:35:51 +0200

    Updated development memcached configuration

--- html/config/phpmemcache/Config/Memcache.php
+++ html/config/phpmemcache/Config/Memcache.php
@@ -1,33 +1,29 @@
 <?php
-return array (
-  'stats_api' => 'Server',
-  'slabs_api' => 'Server',
-  'items_api' => 'Server',
-  'get_api' => 'Server',
-  'set_api' => 'Server',
-  'delete_api' => 'Server',
-  'flush_all_api' => 'Server',
-  'connection_timeout' => '1',
-  'max_item_dump' => '100',
-  'refresh_rate' => 5,
-  'memory_alert' => '80',
-  'hit_rate_alert' => '90',
-  'eviction_alert' => '0',
-  'file_path' => 'Temp/',
-  'servers' => 
-  array (
-    'Default' => 
-    array (
-      '172.18.0.2:11211' => 
-      array (
-        'hostname' => '172.18.0.2',
-        'port' => '11211',
-      ),
-      '172.18.0.3:11211' => 
-      array (
-        'hostname' => '172.18.0.3',
-        'port' => '11211',
-      ),
-    ),
-  ),
-);
+
+return [
+    'stats_api'          => 'Memcached',
+    'slabs_api'          => 'Server',
+    'items_api'          => 'Memcached',
+    'get_api'            => 'Memcached',
+    'set_api'            => 'Memcached',
+    'delete_api'         => 'Memcached',
+    'flush_all_api'      => 'Memcached',
+    'connection_timeout' => '1',
+    'max_item_dump'      => '100',
+    'refresh_rate'       => 5,
+    'memory_alert'       => '80',
+    'hit_rate_alert'     => '90',
+    'eviction_alert'     => '0',
+    'file_path'          => 'Temp/',
+    'servers'            =>
+        [
+            'localhost' =>
+                [
+                    '127.0.0.1:11211' =>
+                        [
+                            'hostname' => '127.0.0.1',
+                            'port'     => '11211',
+                        ],
+                ],
+        ],
+];
