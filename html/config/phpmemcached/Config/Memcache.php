6f77ab99e0feef0332112d156f664bfa1b348458
Toni Ginard <toni.ginard@gmail.com>
2019-11-11 14:15:38 +0100

    Added memcached configuration

---
+++ html/config/phpmemcached/Config/Memcache.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once dirname(dirname(dirname(__FILE__))) . '/env-config.php';
+
+foreach (explode("\n", $agora['moodle2']['memcache_servers']) as $value) {
+    $servers[trim($value)] = [
+        'hostname' => trim($value),
+        'port' => '11211',
+    ];
+}
+
+return [
+    'stats_api' => 'Server',
+    'slabs_api' => 'Server',
+    'items_api' => 'Server',
+    'get_api' => 'Server',
+    'set_api' => 'Server',
+    'delete_api' => 'Server',
+    'flush_all_api' => 'Server',
+    'connection_timeout' => '1',
+    'max_item_dump' => '100',
+    'refresh_rate' => 2,
+    'memory_alert' => '80',
+    'hit_rate_alert' => '90',
+    'eviction_alert' => '0',
+    'file_path' => '/tmp/',
+    'servers' => [
+        'Default' => $servers,
+    ],
+];
