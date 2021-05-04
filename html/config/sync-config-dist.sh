f09f7813b5b5f0d55972b5dfada98d087bb8728e
Toni Ginard <toni.ginard@gmail.com>
2021-05-04 18:35:40 +0200

    sync-config.php: Removed old code

--- html/config/sync-config-dist.sh
+++ html/config/sync-config-dist.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
 
-# DEV
 PHP_PATH=/usr/bin/php
-
-# CMO
-#PHP_PATH="/opt/xtk/php5/bin/php -c /opt/xtk/php5/lib/php.ini"
-
-# FMO AGORA INT / AGORA-EOI INT
-#PHP_PATH="/opt/rh/php54/root/usr/bin/php -c /serveis/conf/int/A02php54/apache/php.ini"
-
-# FMO AGORA PRE
-#PHP_PATH="/opt/rh/php54/root/usr/bin/php -c /serveis/conf/pre/1051/apache/php.ini"
-
-# FMO AGORA PRO
-#PHP_PATH="/opt/rh/php54/root/usr/bin/php -c /serveis/conf/pro/1051/apache/php.ini"
-
-# FMO AGORA-EOI PRE
-#PHP_PATH="/opt/rh/php54/root/usr/bin/php -c /serveis/conf/pre/1143/apache/php.ini"
-
-# FMO AGORA-EOI PRO
-#PHP_PATH="/opt/rh/php54/root/usr/bin/php -c /serveis/conf/pro/1143/apache/php.ini"
